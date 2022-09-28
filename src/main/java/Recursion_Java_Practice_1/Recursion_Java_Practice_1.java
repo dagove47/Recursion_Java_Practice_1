3d4eca60d3a3f566144f6917818be8555b837c1e
David Gomez <dagove47@gmail.com>
2022-09-28 03:34:54 -0600

    Main Menu
    Main Menu added

--- src/main/java/Recursion_Java_Practice_1/Recursion_Java_Practice_1.java
+++ src/main/java/Recursion_Java_Practice_1/Recursion_Java_Practice_1.java
@@ -5,13 +5,57 @@
 
 package Recursion_Java_Practice_1;
 
+import javax.swing.JOptionPane;
+
 /**
  *
- * @author david.b.gomez
+ * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class Recursion_Java_Practice_1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String practiceSelect = MainMenu();        
+        switch (practiceSelect) {
+            case "a":
+                ExerciseA exerciseA  = new ExerciseA();
+                //exerciseA...();
+                break;
+            case "b":
+                ExerciseB exerciseB  = new ExerciseB();
+                //exerciseB...();
+                break;
+            case "c":
+                ExerciseC exerciseC  = new ExerciseC();
+                //exerciseC...();
+                break;
+            case "d":
+                ExerciseD exerciseD  = new ExerciseD();
+                //exerciseD...();
+                break;
+        }
     }
+    
+    
+    public static String MainMenu() {
+        boolean repeat = true;
+        String practiceSelect = "";
+        while (repeat) {
+            practiceSelect = JOptionPane.showInputDialog("Práctica Recursividad \n\n" + 
+                "a. Elimine las vocales de una oración e imprima la oración resultante. \n" +
+                "b. Crear un método que obtenga la suma de los números naturales desde 1 hasta N. \n" +
+                "c. Compruebe si un número es binario. Un número binario está formado únicamente por ceros y unos. \n" +
+                "d. Convierta el siguiente código de ciclos for en funciones recursivas. \n\n" +
+                "Digite  'a',  'b',  'c'  o  'd' \n").toLowerCase();
+            System.out.println(practiceSelect);
+            
+            if((practiceSelect.equals("a")) || (practiceSelect.equals("b")) || (practiceSelect.equals("c")) || (practiceSelect.equals("d"))) {
+                repeat = false;
+            } else {
+                JOptionPane.showMessageDialog(null, "Valor invalido");
+            }
+        }
+        return practiceSelect;
+    }
+    
+    
 }

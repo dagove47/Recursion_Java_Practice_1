42d80965a2a9db3fbf5adb8bfe88aa157493956f
Gabriel-Brilla <114454954+Gabriel-Brilla@users.noreply.github.com>
2022-09-29 13:42:52 -0600

    Update ExerciseB.java
    En este use la formula para conseguir el resultado de la suma de 1 a N

--- src/main/java/Recursion_Java_Practice_1/ExerciseB.java
+++ src/main/java/Recursion_Java_Practice_1/ExerciseB.java
@@ -9,5 +9,9 @@ package Recursion_Java_Practice_1;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class ExerciseB {
-    
+    int n = Integer.parseInt(JOptionPane.showInputDialog("Digite el el "
+        + "hasta el que quiere sumar")); 
+        //usuario digita el numero deseado y se convierte en un int
+        int suma = (n*(n+1))/(2);
+        System.out.println("La respuesta de la suma es : " + suma);
 }

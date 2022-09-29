3c73e72f8bde89b48a182a2c93b35d67848648e2
Gabriel-Brilla <114454954+Gabriel-Brilla@users.noreply.github.com>
2022-09-29 16:11:25 -0600

    Update ExerciseB.java
    Suma recursiva de  1 hasta N 
    (￣﹃￣) (me gustaba mas mi primer comit mas simple ○( ＾皿＾)っ Hehehe… )

--- src/main/java/Recursion_Java_Practice_1/ExerciseB.java
+++ src/main/java/Recursion_Java_Practice_1/ExerciseB.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+import javax.swing.JOptionPane;
 package Recursion_Java_Practice_1;
 
 /**
@@ -9,9 +6,20 @@ package Recursion_Java_Practice_1;
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class ExerciseB {
-    int n = Integer.parseInt(JOptionPane.showInputDialog("Digite el el "
+    
+    public static void main(String[] args) {
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite el el "
         + "hasta el que quiere sumar")); 
-        //usuario digita el numero deseado y se convierte en un int
-        int suma = (n*(n+1))/(2);
-        System.out.println("La respuesta de la suma es : " + suma);
+        int suma = (Adicion(num));
+        System.out.println("El resultado de la suma deseada es = " + suma);
+    }
+    
+    public static int Adicion(int num){
+        if (num != 0){
+            return num + Adicion (num - 1);
+        }
+        else{
+            return num;
+        }
+    }  
 }

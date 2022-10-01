d7aedbfca318f540818a8fd1f5d996e321d5b6c5
alexkun90 <alexander9824@gmail.com>
2022-10-01 08:24:41 -0600

    Ejercicio A Terminado
    Recursividad

--- src/main/java/Recursion_Java_Practice_1/ExerciseA.java
+++ src/main/java/Recursion_Java_Practice_1/ExerciseA.java
@@ -4,14 +4,33 @@
  */
 package Recursion_Java_Practice_1;
 
+import javax.swing.*;
+
 /**
  *
  * @author David Gomez, Gabriel Brilla, Gerald Blanco, Alexander Torres
  */
 public class ExerciseA {
-
-    public static void solucionA(){
-
+    public void solucionA(){
+        //Llamando datos
+        ExerciseA ejeA=new ExerciseA();
+        String cadena = JOptionPane.showInputDialog("Ingrese una palabra: ");
+        char[] array = cadena.toCharArray();
+        array = ejeA.eliminarVocales(0, array);
+        String nuevaCadena = String.valueOf(array);
+        System.out.println("Palabra sin vocales: "+nuevaCadena.replace(" ",""));
     }
-    
+    //Utilizando Recursividad
+    public char[] eliminarVocales(int i, char[] array) {
+        if (i == array.length) {
+            return array;
+        } else {
+            char letra = array[i];
+            if (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u'|| letra == 'A'
+                    || letra == 'E' || letra == 'I' || letra == 'O' || letra == 'U') {
+                array[i] = ' ';
+            }
+            return eliminarVocales(++i, array);
+        }
+    }
 }
